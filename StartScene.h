fbeb146cc304bfffc45d575a3ffa3f3d0b558743
SuperMao2020 <74147893+SuperMao2020@users.noreply.github.com>
2021-05-24 12:43:08 +0800

    Create StartScene.h

---
+++ StartScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "common.h"
+
+//开始界面
+class StartScene
+	: public Scene
+{
+public:
+	StartScene();
+	void Sound();
+protected:
+	// 声音开关
+	Button* soundBtn;
+	// 继续游戏按钮
+	Button* resumeBtn;
+};
+
